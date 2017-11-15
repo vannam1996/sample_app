6fe4d3e141293fe190037839ea68876f4ae46ac2
Van Nam <vannam051096@gmail.com>
2017-11-15 09:57:04 +0700

    Chapter 7: Sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "/signup", to:"user#new"
+  get "/signup", to:"users#new"
 
   root "static_pages#home"
 
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
 
   get "/contact", to:"static_pages#contact"
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post "/signup",  to: "users#create"
+
+  resources :users
 end
